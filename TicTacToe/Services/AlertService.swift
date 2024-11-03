4446dbc1aaf7d8296c0e8ef967ea2b83331dfa73
Sergey Telnov <sergej.telnov@gmail.com>
2024-11-03 18:07:49 +0100

    Added an alert service to show alerts at the end of the game

---
+++ TicTacToe/Services/AlertService.swift
@@ -0,0 +1,32 @@
+//
+//  AlertService.swift
+//  TicTacToe
+//
+//  Created by Sergey Telnov on 03/11/2024.
+//
+
+import Foundation
+import UIKit
+
+final class AlertService {
+    static let shared = AlertService()
+    private init() {}
+    
+    func showAlert(withTitle title: String, withText text: String, on viewController: UIViewController, withOk okTitle: String, withCancel cancelTitle: String? = nil, okAction: (() -> Void)? = nil, cancelAction: (() -> Void)? = nil) {
+        
+        let alert = UIAlertController(title: title, message: text, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: okTitle, style: .default) { _ in
+            okAction?()
+        }
+        alert.addAction(okAction)
+        
+        if let cancelTitle = cancelTitle {
+            let cancelAction = UIAlertAction(title: cancelTitle, style: .default) { _ in
+                cancelAction?()
+            }
+            alert.addAction(cancelAction)
+        }
+        
+        viewController.present(alert, animated: true, completion: nil)
+    }
+}
